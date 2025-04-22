cf5caf56544234b5b9de73c4e93ac3d5139dc8ed
SeregaKazak <sergeykazak2020@gmail.com>
2025-04-22 12:00:56 +0300

    Create OOP_C++_2.cpp

---
+++ OOP_C++_2.cpp
@@ -0,0 +1,95 @@
+﻿#include <iostream>
+#include <string>
+
+class Character {
+private:
+    std::string name;
+    int health;
+    int attack;
+    int defense;
+
+public:
+    // Конструктор
+    Character(const std::string& n, int h, int a, int d)
+        : name(n), health(h), attack(a), defense(d) {
+        std::cout << "Character " << name << " created!\n";
+    }
+
+    // Деструктор
+    ~Character() {
+        std::cout << "Character " << name << " destroyed!\n";
+    }
+
+    void displayInfo() const {
+        std::cout << "Name: " << name << ", HP: " << health
+            << ", Attack: " << attack << ", Defense: " << defense << std::endl;
+    }
+};
+
+class Monster {
+private:
+    std::string name;
+    int health;
+    int attack;
+    int defense;
+
+public:
+    // Конструктор
+    Monster(const std::string& n, int h, int a, int d)
+        : name(n), health(h), attack(a), defense(d) {
+        std::cout << "Monster " << name << " created!\n";
+    }
+
+    // Деструктор
+    ~Monster() {
+        std::cout << "Monster " << name << " destroyed!\n";
+    }
+
+    void displayInfo() const {
+        std::cout << "Name: " << name << ", HP: " << health
+            << ", Attack: " << attack << ", Defense: " << defense << std::endl;
+    }
+};
+
+class Weapon {
+private:
+    std::string name;
+    int damage;
+    double weight;
+
+public:
+    // Конструктор
+    Weapon(const std::string& n, int d, double w)
+        : name(n), damage(d), weight(w) {
+        std::cout << "Weapon " << name << " created!\n";
+    }
+
+    // Деструктор
+    ~Weapon() {
+        std::cout << "Weapon " << name << " destroyed!\n";
+    }
+
+    void displayInfo() const {
+        std::cout << "Name: " << name << ", Damage: " << damage
+            << ", Weight: " << weight << std::endl;
+    }
+};
+
+int main() {
+    Character hero("Hero", 100, 10, 5);
+    Monster goblin("Goblin", 50, 5, 2);
+
+    hero.displayInfo();
+    goblin.displayInfo();
+
+    Weapon sword("Sword", 20, 2.5);
+    Weapon axe("Axe", 30, 4.0);
+    Weapon dagger("Dagger", 10, 1.0);
+
+    sword.displayInfo();
+    axe.displayInfo();
+    dagger.displayInfo();
+
+    return 0;
+}
+
---
+++ OOP_C++_3.cpp
@@ -0,0 +1,92 @@
+﻿#include <iostream>
+#include <string>
+
+class Character {
+private:
+    std::string name;
+    int health;
+    int attack;
+    int defense;
+
+public:
+    Character(const std::string& n, int h, int a, int d)
+        : name(n), health(h), attack(a), defense(d) {
+    }
+
+    // Перегрузка оператора ==
+    bool operator==(const Character& other) const {
+        return name == other.name && health == other.health;
+    }
+
+    // Перегрузка оператора <<
+    friend std::ostream& operator<<(std::ostream& os, const Character& character) {
+        os << "Character: " << character.name << ", HP: " << character.health
+            << ", Attack: " << character.attack << ", Defense: " << character.defense;
+        return os;
+    }
+};
+
+
+class Weapon {
+private:
+    std::string name;
+    int damage;
+    double weight;
+
+public:
+    Weapon(const std::string& n, int d, double w)
+        : name(n), damage(d), weight(w) {
+    }
+
+    int getDamage() const { return damage; }
+    std::string getName() const { return name; }
+    double getWeight() const { return weight; }
+
+    // Перегрузка оператора +
+    Weapon operator+(const Weapon& other) const {
+        return Weapon(this->name + " + " + other.name, this->damage + other.damage, this->weight + other.weight);
+    }
+
+    // Перегрузка оператора >
+    bool operator>(const Weapon& other) const {
+        return damage > other.damage;
+    }
+
+    // Перегрузка оператора << для вывода информации об оружии
+    friend std::ostream& operator<<(std::ostream& os, const Weapon& weapon) {
+        os << "Weapon: " << weapon.getName() << ", Damage: " << weapon.getDamage()
+            << ", Weight: " << weapon.getWeight();
+        return os;
+    }
+};
+
+int main() {
+    Character hero1("Hero", 100, 20, 10);
+    Character hero2("Hero", 100, 20, 10);
+    Character hero3("Warrior", 150, 25, 15);
+
+    if (hero1 == hero2) {
+        std::cout << "Hero1 and Hero2 are the same!\n";
+    }
+    if (!(hero1 == hero3)) {
+        std::cout << "Hero1 and Hero3 are different!\n";
+    }
+
+    std::cout << hero1 << std::endl; // Вывод информации о персонаже
+
+    Weapon sword("Sword", 20, 2.5);
+    Weapon bow("Bow", 15, 1.5);
+    Weapon axe("Axe", 30, 4.0);
+
+    Weapon combinedWeapon = sword + bow;
+    std::cout << "Combined weapon: " << combinedWeapon << std::endl;
+
+    if (axe > sword) {
+        std::cout << "Axe has more damage than Sword!\n";
+    }
+    else {
+        std::cout << "Sword has more damage than Axe!\n";
+    }
+
+    return 0;
+}
